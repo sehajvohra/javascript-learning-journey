cd923a2210fe23dcbad74b5e82dc621a0fe35966
Sehaj Vohra <sehajvohra04@gmail.com>
2026-08-23 14:44:01 +0000

    add dataypes basics

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,4 +2,27 @@
 
 // alert( 3 + 3);  we are using nodejs, not browser
 
-console.log(3 + 3); console.log("Sehaj") // not readable
+console.log(3 
+    +
+     3); // not readable
+
+console.log("Sehaj") 
+
+let name = "Sehaj"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+ 
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
